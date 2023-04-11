cf20cdaab46bc7b88df412dd3afee6e1107abaf9
Brendan T <48305105+brendtay@users.noreply.github.com>
2023-04-11 10:31:29 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser")
 const Pool = require('pg').Pool
+require('dotenv').config()
 
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: true}));
